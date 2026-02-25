050c50e93ad611cf12ec5a5b90a3b6b1580922ba
Sonce <oleksndrkorn@gmail.com>
2026-02-25 17:31:16 +0200

    update providers

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,17 +1,17 @@
-import { PropsWithChildren, useMemo } from "react";
-import {
-  ConnectionProvider,
-  WalletProvider as SolanaWalletProvider,
-} from "@solana/wallet-adapter-react";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { PropsWithChildren } from "react";
+// import {
+//   // ConnectionProvider,
+//   WalletProvider as SolanaWalletProvider,
+// } from "@solana/wallet-adapter-react";
+// import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { clusterApiUrl } from "@solana/web3.js";
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-  UnsafeBurnerWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
+// import { clusterApiUrl } from "@solana/web3.js";
+// import {
+//   PhantomWalletAdapter,
+//   SolflareWalletAdapter,
+//   UnsafeBurnerWalletAdapter,
+// } from "@solana/wallet-adapter-wallets";
 
 import "@solana/wallet-adapter-react-ui/styles.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -19,28 +19,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 export const WalletProvider = ({ children }: PropsWithChildren) => {
-  const network = WalletAdapterNetwork.Devnet;
-
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter(),
-      new UnsafeBurnerWalletAdapter(),
-    ],
-    []
-  );
 
   return (
-    <ConnectionProvider endpoint={endpoint}>
-      <SolanaWalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          <QueryClientProvider client={queryClient}>
-            {children}
-          </QueryClientProvider>
-        </WalletModalProvider>
-      </SolanaWalletProvider>
-    </ConnectionProvider>
+    // <ConnectionProvider>
+    // <SolanaWalletProvider>
+    <WalletModalProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </WalletModalProvider>
+    // </SolanaWalletProvider>
+    // </ConnectionProvider>
   );
 };
